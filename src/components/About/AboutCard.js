2780dfef8059208c9bb33234237b7bb58dc3eff3
Hamzaiftikhhar <hamzaiftikhhar@gmail.com>
2025-02-16 00:27:21 +0500

    Added the particles functionality

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">ALI HAMZA </span>  
+            Hi Everyone, I am <span className="purple">Ali Hamza </span>  
             from <span className="purple">Lahore, Pakistan.</span>
             <br />
             I am currently in my 7th semester, pursuing a Bachelor's degree in Computer Science.
@@ -33,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Keep building, keep learning, and make an impact!"{" "}
           </p>
-          <footer className="blockquote-footer">Hamza Iftikhhar</footer>
+          <footer className="blockquote-footer">Ali Hamza</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,6 @@ function Footer() {
           <h3>Designed and Developed by ALI HAMZA</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
